87fb9e94ad661cf7eef13c22f06d5817996187d5
Derek Stegelman <dstegelman@gmail.com>
2012-03-10 16:33:35 -0800

    Adding decorators.

--- tailor/decorators.py
+++ tailor/decorators.py
@@ -2,3 +2,11 @@ def tailored(func):
     ''' This decorator marks a fabric method as being viewable to tailor.'''
     func.tailored = True
     return func
+    
+def change_run_to_local(func):
+    ''' Change any run method to local in the method. '''
+    
+    func_map = {}
+    func_map['run'] = 'local'
+    
+    return func
