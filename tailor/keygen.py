88c6966e45a651a2db51a56696ab720477ee6780
Garrett Pennington <garrettp@gmail.com>
2012-03-12 15:08:45 -0700

    added keygen utility

---
+++ tailor/keygen.py
@@ -0,0 +1,6 @@
+import random
+
+def generate(length=32):
+    key = ''.join(random.choice('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(length))
+    return key
+
