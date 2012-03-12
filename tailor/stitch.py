4b85e69fcdc49462181df738ce717a5a585dd05b
Derek Stegelman <dstegelman@gmail.com>
2012-03-12 14:27:30 -0700

    Adding shrit class.

---
+++ tailor/stitch.py
@@ -0,0 +1,57 @@
+import random
+import pickle
+from fabric.api import execute as fab_exec
+
+class Shirt:
+    file_name = "fab_temp.py"
+    
+    def __init__(self):
+        pass
+        
+    def setup(self):
+        fabfile = open(self.file_name, "w")
+        new_string = ""
+        new_string = new_string + "from fabric.api import *\nfrom tailor.decorators import *\n\n\nimport fabric\n\n\n"
+        
+        
+        new_string = new_string + '''fabric.state.output["status"] = False\nfabric.state.output["running"] = False\nfabric.state.output["user"] = False\nfabric.state.output["warnings"] = False\nfabric.state.output["stderr"] = False\nfabric.state.output['stdout'] = False\nfabric.state.output['aborts'] = False\n\n'''
+        fabfile.write(new_string)
+        fabfile.close()
+    
+    def add_vars(self, env_dict):
+        new_string = ""
+        for _varname, _var in env_dict.iteritems():
+            if isinstance(_var, str):
+                new_string = new_string + "env.%s = \"%s\"" % (_varname, _var) + "\n"
+            else:
+                new_string = new_string + "env.%s = %s" % (_varname, _var) + "\n"
+        new_string = new_string + "\n\n"
+        
+        fabfile = open(self.file_name, "a")
+        fabfile.write(new_string)
+        fabfile.close()
+        
+    def add_methods(self, method_dict):
+        new_string = ""
+        for method_name, method in method_dict:
+            new_string = new_string + pickle.loads(str(method)) + "\n\n"
+        fabfile = open(self.file_name, "a")
+        fabfile.write(new_string)
+        fabfile.close()
+        
+    def execute(self, commands):
+        import fab_temp
+        for command in commands:
+            print command
+            try:
+                yo_dog = fab_exec(eval("fab_temp." + command))
+                print("output")
+                print yo_dog
+            except AttributeError:
+                return False
+        return True
+        
+    def cleanup(self):
+        import os
+        os.remove(fab_temp.py)
+        
