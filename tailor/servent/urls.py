8823403735c54e1e1002a739cfd8c8576460406d
Garrett Pennington <garrettp@gmail.com>
2012-08-12 11:12:30 -0500

    split code into servent app

--- tailor/servent/urls.py
+++ tailor/servent/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
-from tailor.views import *
+from tailor.servent.views import *
 
 urlpatterns = patterns('',    
-    url(r'^api/v1/schema/$', schema, name='schema'),
     url(r'^api/v1/fab/$', fab, name='fab'),
-
 )
 
