5e6932fa188f3eb74cd3096da1184ad44584a615
Garrett Pennington <garrettp@gmail.com>
2012-08-14 22:32:29 -0500

    added static dir with js and jquery-ui

---
+++ tailor/servent/static/servent/js/tailor.js
@@ -0,0 +1,64 @@
+//Tailor
+function build_fab(_key, _url){
+
+    //build fab command
+    $('form').on('change', 'select', function(){
+        // TODO Account for # too
+        if (($(this).val() != '---')){
+            $(this).clone().appendTo('.terminal');
+        }
+    });
+
+    $('#execute').click(function(e){
+
+        e.preventDefault();
+
+        //Create Dialog
+    	$('#dashboard-terminal').dialog({
+    		modal: true,
+    		draggable: false,
+    		resizable: false,
+    		width: 500,
+    		height: 400,
+    		title: "Terminal",
+    		buttons: {
+    			Ok: function() {
+    				$( this ).dialog( "close" );
+    			}
+    		}
+    	});
+
+        //Create POST Data object
+        post_data = {
+            "hosts": [],
+            "commands": [],
+            "api_key": _key,
+        }
+        
+        //Get tailor selects, all but last child (----)
+        //Create Command objects
+        var selects = $('.tailor-terminal-select:not(:last-child)');
+        $.each(selects, function(index, item){
+            task = { 'command': $(item).children('option:selected').val(), 'params': [] }
+            post_data['commands'].push(task);
+        })
+
+        //Make the async call
+        //Display responses in dialog
+    	$.ajax({
+    		type: "POST",
+    		timeout: 600000,
+    		url: _url,
+    		dataType: 'json',
+    		data: JSON.stringify(post_data),
+            success: function(data){
+                //console.log(data.responses)
+                data.responses.forEach(function(item){
+                    $('#dashboard-terminal').append("<p>" + item.response + "</p>")
+                })
+            },
+    	 });
+        //end execute fab command 
+
+    });
+}
